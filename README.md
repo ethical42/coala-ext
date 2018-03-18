403151c8154e4f530aff1ee9e39d171e5de3007d
ethical42 <tonipaajic@gmail.com>
2018-03-18 05:31:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google Chrome Extension
+This is a Google Chrome extension to be able to make a custom new tab page.
---
+++ script.js
@@ -0,0 +1,13 @@
+function data() {
+	var datetime = moment().lang("en").format('MMMM Do YYYY @ h:mm:ss');
+	
+	$(".datetime").html(datetime);
+}
+
+$(function() {
+	
+	data();
+
+	setInterval(data, 1000);
+
+});
