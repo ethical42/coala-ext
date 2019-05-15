93c96bbe829bd49a4b3b63d6892c7f6f84dcfb9b
ethical42 <tonipaajic@gmail.com>
2019-05-15 11:20:04 +0200

    Added functionality to clear cache and history

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 function data() {
 	var datetime = moment().lang("en").format('MMMM Do YYYY @ h:mm:ss');
-	
+
 	$(".datetime").html(datetime);
 }
 
 $(function() {
-	data();
+	// Initiate data();
+    data();
 
+	// New tab
 	setInterval(data, 1000);
-});
 
+	// Clear last two hours of history
+	$(".clear-history").on("click", function() {
+        var ms = (60 * 60 * 60) * 1000; // 2 hours
+        var twoHoursAgo = (new Date()).getTime() - ms;
+        chrome.browsingData.remove({
+          "since": twoHoursAgo
+        }, {
+          "history": true,
+        });
+
+        alert("Last 2 hours of history cleared successfully!");
+	});
+
+    // Clear last two hours of cache
+    $(".clear-cache").on("click", function() {
+        var ms = (60 * 60 * 60) * 1000; // 2 hours
+        var twoHoursAgo = (new Date()).getTime() - ms;
+        chrome.browsingData.remove({
+          "since": twoHoursAgo
+        }, {
+          "cache": true,
+        });
+
+        alert("Last 2 hours of cache cleared successfully!");
+	});
+
+    // Clear last two hours of all cache and history
+    $(".clear-all").on("click", function() {
+        chrome.browsingData.remove({
+          "since": 0
+        }, {
+          "history": true,
+          "cache": true
+        });
+
+        alert("All cache and history cleared successfully!");
+	});
+});
